c2957e2ed6b2eb3d86dc51c591fb1fe6c99e0fc1
Xuan Yang <xuan.yang@goldenf.co>
2020-12-22 17:58:35 +0800

    update interface

--- src/Contract/BoleCardServiceInterface.php
+++ src/Contract/BoleCardServiceInterface.php
@@ -7,115 +7,32 @@ namespace GiocoPlus\BoleCard\Contract;
 interface BoleCardServiceInterface {
 
     /**
-     * 錢包餘額
+     * 遊戲啟動
      * @param string $account_with_op
      * @param string $delimiter
-     * @param string $wallet_code
-     * @return array
+     * @param string $game_code
+     * @return mixed
      */
-    function getBalance(string $account_with_op, string $delimiter, string $wallet_code): array;
-    /**
-     * 上分
-     * @param string $account_with_op
-     * @param string $wallet_code
-     * @param float $amount
-     * @param string $trace_id
-     * @return array
-     */
-    function opTransferIn(string $account_with_op, string $delimiter, string $wallet_code, float $amount, string $trace_id): array;
+    function gameLaunch(string $account_with_op, string $delimiter, string $game_code);
 
     /**
-     * 下分
-     * @param string $account_with_op
-     * @param string $wallet_code
-     * @param float $amount
-     * @param string $trace_id
-     * @return array
-     */
-    function opTransferOut(string $account_with_op, string $delimiter, string $wallet_code, float $amount, string $trace_id): array;
-
-    /**
-     * 遊戲上分
-     * @param string $account_with_op
-     * @param string $wallet_code
-     * @param float $amount
-     * @param string $trace_id
-     * @return array
-     */
-    function gameTransferIn(string $account_with_op, string $delimiter, string $wallet_code, float $amount, string $trace_id): array;
-
-    /**
-     * 遊戲下分
-     * @param string $account_with_op
-     * @param string $wallet_code
-     * @param float $amount
-     * @param string $trace_id
-     * @return array
-     */
-    function gameTransferOut(string $account_with_op, string $delimiter, string $wallet_code, float $amount, string $trace_id): array;
-
-    /**
-     * 下注
-     * @param string $account_with_op
-     * @param string $wallet_code
-     * @param float $amount
-     * @param string $trace_id
-     * @param string $bet_id
-     * @return array
-     */
-    function gameStake(string $account_with_op, string $delimiter, string $wallet_code, float $amount, string $trace_id, string $bet_id): array;
-
-    /**
-     * 派彩
-     * @param string $account_with_op
-     * @param string $wallet_code
-     * @param float $amount
-     * @param string $trace_id
-     * @param string $bet_id
-     * @return array
-     */
-    function gamePayoff(string $account_with_op, string $delimiter, string $wallet_code, float $amount, string $trace_id, string $bet_id): array;
-
-    /**
-     * 取消下注
-     * @param string $account_with_op
-     * @param string $wallet_code
-     * @param float $amount
-     * @param string $trace_id
-     * @param string $bet_id
-     * @return array
-     */
-    function cancelStake(string $account_with_op, string $delimiter, string $wallet_code, float $amount, string $trace_id, string $bet_id): array;
-
-    /**
-     * 取消派彩
-     * @param string $account_with_op
-     * @param string $wallet_code
-     * @param float $amount
-     * @param string $trace_id
-     * @param string $bet_id
-     * @return array
-     */
-    function cancelPayoff(string $account_with_op, string $delimiter, string $wallet_code, float $amount, string $trace_id, string $bet_id): array;
-
-    /**
-     * 調整資金
-     * @param string $account_with_op
-     * @param string $wallet_code
-     * @param float $amount
-     * @param string $trace_id
-     * @param string $memo
-     * @return array
-     */
-    function adjust(string $account_with_op, string $delimiter, string $wallet_code, float $amount, string $trace_id, string $memo): array;
-
-    /**
-     * 查詢訂單
+     * 遊戲詳情
      * @param string $account_with_op
      * @param string $delimiter
      * @param string $trace_id
-     * @return array
+     * @return mixed
      */
-    function queryOrder(string $account_with_op, string $delimiter, string $trace_id): array;
+    function gameDetail(string $account_with_op, string $delimiter, string $bet_id);
+
+    /**
+     * 遊戲紀錄
+     * @param string $op_code
+     * @param int $start_time
+     * @param int $end_time
+     * @param int $page
+     * @param int $page_size
+     * @return mixed
+     */
+    function gameRecord(string $op_code, int $start_time, int $end_time, int $page, int $page_size);
 }
 
