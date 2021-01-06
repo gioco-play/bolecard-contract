92d6eccdbf2a8cb8664502375c7cb6f9c6cab348
Xuan Yang <xuan.yang@goldenf.co>
2021-01-06 11:36:07 +0800

    add field

--- src/Contract/BoleCardServiceInterface.php
+++ src/Contract/BoleCardServiceInterface.php
@@ -18,10 +18,11 @@ interface BoleCardServiceInterface {
     /**
      * 遊戲詳情
      * @param string $op_code
+     * @param string $account
      * @param string $bet_id
      * @return mixed
      */
-    function gameDetail(string $op_code, string $bet_id);
+    function gameDetail(string $op_code, string $account, string $bet_id);
 
     /**
      * 遊戲紀錄
